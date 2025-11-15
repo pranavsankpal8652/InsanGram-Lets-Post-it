0228601cdd55991ec9f36f3be51b9cde3f60f966
Pranav Sankpal <162540013+pranavsankpal8652@users.noreply.github.com>
2025-11-15 20:38:22 +0530

    fixed wrong import for production

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,34 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import Register from './Components/Register.jsx'
-import AuthProvider from './context/AuthContext.jsx'
-import { ToastContainer } from 'react-toastify'
-import Forgotpass from './Components/forgotpass.jsx'
-import Dashboard from './Components/Dashboard.jsx'
-import Home from './Components/Home.jsx'
-import PostCreation from './Components/User_Pages/PostCreation.jsx'
-import UserPostsManager from './Components/User_Pages/ManagePosts.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Register from "./Components/Register.jsx";
+import AuthProvider from "./context/AuthContext.jsx";
+import { ToastContainer } from "react-toastify";
+import Forgotpass from "./Components/ForgotPass.jsx";
+import Dashboard from "./Components/Dashboard.jsx";
+import Home from "./Components/Home.jsx";
+import PostCreation from "./Components/User_Pages/PostCreation.jsx";
+import UserPostsManager from "./Components/User_Pages/ManagePosts.jsx";
 
-createRoot(document.getElementById('root')).render(
-
-
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ToastContainer />
     <AuthProvider>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/forgotpass/:token?' element={<Forgotpass />}></Route>
-        <Route path='/Register' element={<Register />}></Route>
-        <Route path='/Dashboard' element={<Dashboard />}></Route>
-        <Route path='/create/:id?' element={<PostCreation />}></Route>
-        <Route path='/manage' element={<UserPostsManager />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/forgotpass/:token?" element={<Forgotpass />}></Route>
+        <Route path="/Register" element={<Register />}></Route>
+        <Route path="/Dashboard" element={<Dashboard />}></Route>
+        <Route path="/create/:id?" element={<PostCreation />}></Route>
+        <Route path="/manage" element={<UserPostsManager />}></Route>
       </Routes>
     </AuthProvider>
-
-
-
   </BrowserRouter>
-
-)
+);
--- server/app/Middleware/Auth.js
+++ server/app/Middleware/Auth.js
@@ -1,97 +1,93 @@
-const { userModel } = require("../model/UsersModel")
-const bcrypt = require('bcryptjs')
+const { userModel } = require("../model/UsersModel");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-require('dotenv').config()
+require("dotenv").config();
 
 let checkUser = async (req, res, next) => {
-    const { username, email, password } = req.body
-    // console.log(req.body)
-    if(username=='' || email=='' || password=='')  return res.status(400).json({ message: "All Fields Are Mandatory!" });
-    try {
-        const userEmailExist = await userModel.findOne({ userEmail: email })
-        const userNameExist = await userModel.findOne({ userName: username })
-        if (userEmailExist || userNameExist) return res.status(400).json({ message: "User already exists" });
-
-
-    }
-    catch (err) {
-        console.log(err)
-    }
-    next()
-
-
-}
+  const { username, email, password } = req.body;
+  // console.log(req.body)
+  if (username == "" || email == "" || password == "")
+    return res.status(400).json({ message: "All Fields Are Mandatory!" });
+  try {
+    const userEmailExist = await userModel.findOne({ userEmail: email });
+    const userNameExist = await userModel.findOne({ userName: username });
+    if (userEmailExist || userNameExist)
+      return res.status(400).json({ message: "User already exists" });
+  } catch (err) {
+    console.log(err);
+  }
+  next();
+};
 
 let findUser = async (req, res, next) => {
-    const { username, password } = req.body
-    if((username|| password)=='')  return res.status(400).json({ message: "All Fields Are Mandatory!" });
+  const { username, password } = req.body;
+  if ((username || password) == "")
+    return res.status(400).json({ message: "All Fields Are Mandatory!" });
 
-    try {
-        const user = await userModel.findOne({ userName: username })
-        if (!user) return res.status(405).json({ message: "User Not found!" });
+  try {
+    const user = await userModel.findOne({ userName: username });
+    if (!user) return res.status(405).json({ message: "User Not found!" });
 
-        const isMatch = await bcrypt.compare(password, user.password)
-        if (!isMatch) return res.status(400).json({ message: "Invalid credentials" })
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
 
-        req.user = user
+    req.user = user;
+  } catch (err) {
+    console.log(err);
+  }
+  next();
+};
 
+const check_email = async (req, res, next) => {
+  const { email } = req.body;
+  if (email === "")
+    return res.status(400).json({ message: "Email is Mandatory!" });
 
-    }
-    catch (err) {
-        console.log(err)
-    }
-    next()
+  try {
+    const user = await userModel.findOne({ userEmail: email });
+    if (!user) return res.status(405).json({ message: "User Not found!" });
 
-}
-
-const check_email=async(req,res,next)=>{
-    const { email } = req.body
-    if(email==='')  return res.status(400).json({ message: "Email is Mandatory!" });
-
-    try {
-        const user = await userModel.findOne({ userEmail: email })
-        if (!user) return res.status(405).json({ message: "User Not found!" });
-
-        req.user = user
-
-    }
-    catch (err) {
-        console.log(err)
-    }
-    next()
-
-}
-
+    req.user = user;
+  } catch (err) {
+    console.log(err);
+  }
+  next();
+};
 
 const verifyToken = (req, res, next) => {
-    const token = req.header("Authorization")?.split(" ")[1];
+  const token = req.header("Authorization")?.split(" ")[1];
 
-    if (!token) return res.status(401).json({ message: "Access Denied" });
+  if (!token) return res.status(401).json({ message: "Access Denied" });
 
-    try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        req.user = verified; // Attach user data to request
-        // console.log(req.user)
-        next();
-    } catch (error) {
-        res.status(400).json({ message: "Invalid Token.Try Login Again" });
-    }
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = verified; // Attach user data to request
+    // console.log(req.user)
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid Token.Try Login Again" });
+  }
 };
 
 const verifyToken_for_resetPass = (req, res, next) => {
-    const token = req.header("Authorization")?.split(" ")[1];
+  const token = req.header("Authorization")?.split(" ")[1];
 
-    if (!token) return res.status(401).json({ message: "Access Denied" });
+  if (!token) return res.status(401).json({ message: "Access Denied" });
 
-    try {
-        const verified=jwt.verify(token, process.env.JWT_SECRET_KEY);
-        req.user=verified
-        next();
-    } catch (error) {
-        res.status(400).json({ message: "Invalid Token.Try Getting Link Again" });
-    }
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid Token.Try Getting Link Again" });
+  }
 };
 
-
-
-module.exports = { checkUser, findUser,verifyToken,verifyToken_for_resetPass,check_email }
+module.exports = {
+  checkUser,
+  findUser,
+  verifyToken,
+  verifyToken_for_resetPass,
+  check_email,
+};
